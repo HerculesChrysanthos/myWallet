6ada7f8f1faadeeb9e68f53f90987c4a9f79becb
Hercules Chrysanthos <h.chrysanthos>
2023-02-10 23:15:50 +0200

    Add transaction model

---
+++ api/transaction/transaction.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { TRANSACTION_TYPES, TRANSACTION_CATEGORIES } = require('../constants');
+
+const transactionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: TRANSACTION_TYPES,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: TRANSACTION_CATEGORIES,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
