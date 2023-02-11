8b11c8d1628a1a7bcf28b89db5eb452bf4b6039f
Hercules Chrysanthos <h.chrysanthos>
2023-02-11 16:34:48 +0200

    Add register and login endpoints

--- api/user/user.route.js
+++ api/user/user.route.js
@@ -1,6 +1,16 @@
+const express = require('express');
 const router = express.Router();
-const validate = require('../middleware/validator');
+const { validate } = require('../middleware/validator');
+const auth = require('../middleware/check-auth');
+const userValidator = require('./user.validator');
+const controller = require('./user.controller');
 
-module.exports = {
-  router,
-};
+router.post(
+  '/register',
+  validate(userValidator.registerSchema),
+  controller.register
+);
+
+router.post('/login', validate(userValidator.loginSchema), controller.login);
+
+module.exports = router;
